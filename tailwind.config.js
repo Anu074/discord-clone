c3de1d204aa8b01f59a229ae2ff6ac2c29bf2d96
Anu074 <psarojanurag@gmail.com>
2023-09-29 12:30:42 +0530

    added files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  fontFamily:['Whitney'],
+  Whitney:["whiteney"],
+  Ginto:["Ginto"],
+  ggSans:["ggSans"]
+}
+
